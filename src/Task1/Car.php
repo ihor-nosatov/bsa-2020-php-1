2d13aae0fad61bdeb5ef119eb4400942b39d4b8f
ihor-nosatov <igornosatov@yahoo.com>
2020-05-11 10:54:25 +0300

    finish class Car

--- src/Task1/Car.php
+++ src/Task1/Car.php
@@ -15,41 +15,47 @@ class Car
         float $fuelConsumption,
         float $fuelTankVolume
     ) {
-        //@todo
+        $this->id = $id;
+        $this->image = $image;
+        $this->name = $name;
+        $this->speed = $speed;
+        $this->pitStopTime = $pitStopTime;
+        $this->fuelConsumption = $fuelConsumption;
+        $this->fuelTankVolume = $fuelTankVolume;
     }
 
     public function getId(): int
     {
-        // @todo
+        return $this->id;
     }
 
     public function getImage(): string
     {
-        // @todo
+        return $this->image;
     }
 
     public function getName(): string
     {
-        // @todo
+        return $this->name;
     }
 
     public function getSpeed(): int
     {
-        // @todo
+        return $this->speed;
     }
 
     public function getPitStopTime(): int
     {
-        // @todo
+        return $this->pitStopTime;
     }
 
     public function getFuelConsumption(): float
     {
-        // @todo
+        return $this->fuelConsumption;
     }
 
     public function getFuelTankVolume(): float
     {
-        // @todo
+        return $this->fuelTankVolume;
     }
 }
