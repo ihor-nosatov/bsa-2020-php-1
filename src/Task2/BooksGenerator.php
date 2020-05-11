b3f66df7be3ac89e45b7f4c319a7f1011bc347e5
ihor-nosatov <igornosatov@yahoo.com>
2020-05-11 11:49:52 +0300

    finish Class BooksGenerator

--- src/Task2/BooksGenerator.php
+++ src/Task2/BooksGenerator.php
@@ -6,8 +6,32 @@ namespace App\Task2;
 
 class BooksGenerator
 {
+
+    public function __construct(
+        int $minPagesNumber,
+        array $libraryBooks,
+        int $maxPrice,
+        array $magazineBooks
+    )
+    {
+        $this->minPagesNumber = $minPagesNumber;
+        $this->libraryBooks = $libraryBooks;
+        $this->maxPrice = $maxPrice;
+        $this->magazineBooks = $magazineBooks;
+    }
+
     public function generate(): \Generator
     {
-        //@todo
+        foreach ($this->libraryBooks as $libraryBook) {
+            if ($libraryBook->getPagesNumber() >= $this->minPagesNumber) {
+                yield $libraryBook;
+            }
+        }
+
+        foreach ($this->magazineBooks as $magazineBook) {
+            if ($magazineBook->getPrice() < $this->maxPrice) {
+                yield $magazineBook;
+            }
+        }
     }
 }
