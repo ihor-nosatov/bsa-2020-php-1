1a61c8334a8b8313cf401900c4f30fcf898ae3e0
ihor-nosatov <igornosatov@yahoo.com>
2020-05-11 11:18:50 +0300

    finish Class Track

--- src/Task1/Track.php
+++ src/Task1/Track.php
@@ -8,31 +8,52 @@ class Track
 {
     public function __construct(float $lapLength, int $lapsNumber)
     {
-        //@todo
+        $this->lapLength = $lapLength;
+		$this->lapsNumber = $lapsNumber;
     }
 
     public function getLapLength(): float
     {
-        // @todo
+        return $this->lapLength;
     }
 
     public function getLapsNumber(): int
     {
-        // @todo
+        return $this->lapsNumber;
     }
 
     public function add(Car $car): void
     {
-        // @todo
+        $this->cars[] = $car;
     }
 
     public function all(): array
     {
-        // @todo
+        return $this->cars;
     }
 
-    public function run(): Car
-    {
-        // @todo
+    public function run(): Car{
+        $carTime = [];
+        $cars = $this->all();
+
+        foreach ($cars as $key => $car){
+            
+            $id = $car->getId();
+            $image = $car->getImage();
+            $name = $car->getName();
+
+            $carSpeed = $car->getSpeed();
+
+            $fuelTankVolume = $car->getFuelTankVolume();
+            $consumption = $car->getFuelConsumption();
+            $pitStop = $car->getPitStopTime();
+
+            $road = $this->lapsNumber * $this->lapLength;
+            $wastedTime = round($road / $carSpeed,3)*3600;
+            
+            $wastedTime += $pitStop * (ceil ((( $road * $consumption * 0.01) / $fuelTankVolume))-1);
+            $carTime[$key] = $wastedTime;
+        }
+        return $this->all()[array_search(min($carTime), $carTime)];
     }
 }
